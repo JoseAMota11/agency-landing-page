90b3e753cfbb888af5fe832771d3c584f94f6f8e
José Mota <jose.a.mota11@gmail.com>
2025-10-17 16:37:17 -0400

    :lipstick: Styling navbar component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,32 @@
+import './navbar.css';
+import Logo from '../assets/logo.svg';
+
 function Navbar() {
-  return <nav>navbar</nav>;
+  return (
+    <nav className="navbar">
+      <header className="navbar-header">
+        <img src={Logo} alt="Logo" className="navbar-header--logo" />
+      </header>
+      <div className="navbar-sections">
+        <a href="#" className="navbar-sections--link active">
+          Home
+        </a>
+        <a href="#" className="navbar-sections--link">
+          About Us
+        </a>
+        <a href="#" className="navbar-sections--link">
+          Our App
+        </a>
+        <a href="#" className="navbar-sections--link">
+          Contacts
+        </a>
+      </div>
+      <div className="navbar-actions">
+        <button className="navbar-actions--login">Log in</button>
+        <button className="navbar-actions--signup">Sign up</button>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
