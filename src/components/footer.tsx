d6aebf678d94b20d4c0c3131c4bf6eaf2c63fcf1
José Mota <jose.a.mota11@gmail.com>
2025-10-18 22:00:20 -0400

    :lipstick: Styling footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,39 @@
+import './footer.css';
+import Logo from '../assets/logo.svg';
+
 function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="footer">
+      <div className="footer--container">
+        <img src={Logo} alt="Logo" className="footer--logo" />
+        <p className="footer--copyright">
+          &copy; {new Date().getFullYear()} Epixelab. All rights reserved.
+        </p>
+        <div className="footer-link--container">
+          <a href="#" className="footer-link--item">
+            Portfolio
+          </a>
+          <a href="#" className="footer-link--item">
+            How it Works
+          </a>
+          <a href="#" className="footer-link--item">
+            Pricing
+          </a>
+          <a href="#" className="footer-link--item">
+            About
+          </a>
+          <a href="#" className="footer-link--item">
+            Login
+          </a>
+        </div>
+      </div>
+      <div className="footer--divider" />
+      <p className="footer--disclaimer">
+        Startup Framework contains components and complex blocks which can
+        easily be integrated into almost any design.
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
